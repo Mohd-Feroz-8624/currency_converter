119d6152b57c04cd64241ba71de396b8ef66c802
Mohd-Feroz-8624 <mohamedferoz2023.com>
2025-07-26 19:27:22 +0530

    default values removed

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { InputBox } from './components/index.js'
 
 
 function App() {
-  const [amount, setAmount] = useState(10)
+  const [amount, setAmount] = useState(0)
   const [from, setFrom] = useState('usd')
   const [to, setTo] = useState('inr')
   const [convertedAmount, setConvertedAmount] = useState(0)
