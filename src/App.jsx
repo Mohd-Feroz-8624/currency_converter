612ebc5654a302bfdb64ee5f108a77406d31bb18
Mohd-Feroz-8624 <mohamedferoz2023.com>
2025-07-26 19:20:54 +0530

    bgs are set

--- src/App.jsx
+++ src/App.jsx
@@ -27,10 +27,10 @@ function App() {
 
   return (
     <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat'
-      style={{ backgroundImage: `url(https://images.pexels.com/photos/4497591/pexels-photo-4497591.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)` }}
+      style={{ backgroundImage: `url(https://images.pexels.com/photos/281260/pexels-photo-281260.jpeg)` }}
     >
       <div className='w-full'>
-        <div className='w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30'>
+        <div className='w-full max-w-md mx-auto shadow-[5px_5px_10px_rgba(0,0,0,0.55)] rounded-lg p-5 backdrop-blur-sm bg-white/50'>
           <form onSubmit={(e) => {
             e.preventDefault()
             convert()
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -30,7 +30,7 @@ function InputBox({
       <div className='w-1/2 flex flex-wrap justify-end text-right'>
         <p className="text-black/40 mb-2 w-full">Currency Type</p>
         <select
-          className='border  rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none'
+          className='border  rounded-lg px-1 py-1 bg-blue-100 cursor-pointer outline-none'
           value={selectedCurrency}
           onChange={(e) => { onCurrencyChange && onCurrencyChange(e.target.value) }}
           disabled={currencyDisabled}
